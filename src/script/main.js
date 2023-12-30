6f2f2cad35de8d2f6499a1c94e7092d071e992a5
joaoleandro <new.joaoleandro@gmail.com>
2023-12-30 14:55:48 -0300

    jogo funcionando

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,5 @@
 const pikachu = document.querySelector(".pikachu")
-const arvore = document.querySelector(".arvore")
+const pokebola = document.querySelector(".pokebola")
 
 const pulo = () => {
     pikachu.classList.add("pulo-pikachu")
@@ -10,12 +10,17 @@ const pulo = () => {
 }
 
 const loop = setInterval(() => {
-    const arvorePosition = arvore.offsetLeft
+    const pokebolaPosition = pokebola.offsetLeft
     const pikachuPosition = Number(window.getComputedStyle(pikachu).bottom.replace('px', ''))
 
-    if(arvorePosition <= 120 && pikachuPosition < 80 && arvorePosition < 40) {
-        arvore.style.animation = "none"
-        arvore.style.left = `${arvorePosition}px`
+    if(pokebolaPosition <= 120 && pikachuPosition < 80 && pokebolaPosition > 50) {
+        pokebola.style.animation = "none"
+        pokebola.style.left = `${pokebolaPosition}px`
+
+        pikachu.style.animation = "none"
+        pikachu.style.bottom = `${pikachuPosition}px`
+        pikachu.src = "./src/images/game-over.png"
+        pikachu.style.width = "150px"
     }
 }, 10)
 
