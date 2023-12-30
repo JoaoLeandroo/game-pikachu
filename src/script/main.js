77f31738363ac46f094ee380779993b7b608609c
joaoleandro <new.joaoleandro@gmail.com>
2023-12-30 17:12:05 -0300

    points

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,7 @@
 const pikachu = document.querySelector(".pikachu")
 const pokebola = document.querySelector(".pokebola")
-
+let contador = document.querySelector(".contador")
+let pontos = 0
 const pulo = () => {
     pikachu.classList.add("pulo-pikachu")
 
@@ -9,9 +10,13 @@ const pulo = () => {
     }, 500);
 }
 
+
+
 const loop = setInterval(() => {
     const pokebolaPosition = pokebola.offsetLeft
     const pikachuPosition = Number(window.getComputedStyle(pikachu).bottom.replace('px', ''))
+    pontos++
+    contador.innerHTML = pontos
 
     if(pokebolaPosition <= 120 && pikachuPosition < 80 && pokebolaPosition > 50) {
         pokebola.style.animation = "none"
@@ -21,6 +26,7 @@ const loop = setInterval(() => {
         pikachu.style.bottom = `${pikachuPosition}px`
         pikachu.src = "./src/images/game-over.png"
         pikachu.style.width = "150px"
+        clearInterval(loop)
     }
 }, 10)
 
